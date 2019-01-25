60c88e96b7701139e5cacb66f5338eb0c135b17c
ruslan.nafisovich@gmail.com <ruslan.nafisovich@gmail.com>
2019-01-25 23:02:39 +0300

    Отредактирована модель из поста группы

--- Movies.Api/ResourceModels/InfoPost.cs
+++ Movies.Api/ResourceModels/InfoPost.cs
@@ -20,5 +20,35 @@ namespace Movies.Api.ResourceModels
     public int id { get; set; }
 
     public string text { get; set; }
+
+    public Attachments[] attachments { get; set; }
   }
+
+    public class Attachments
+    {
+        public string type { get; set; }
+
+        public Video video { get; set; }
+
+        public Photo photo { get; set; }
+    }
+
+    public class Photo
+    {
+        public Size[] sizes { get; set; }
+    }
+
+    public class Size
+    {
+        public string url { get; set; }
+    }
+
+    public class Video
+    {
+        public int id { get; set; }
+
+        public int owner_id { get; set; }
+
+        public string title { get; set; }
+    }
 }
